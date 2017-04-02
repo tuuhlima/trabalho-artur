e7503d835677a065548d93b7527ee083fc4ab9e6
Túlio Lima <tuuhlima@yahoo.com.br>
2017-04-02 16:37:09 -0300

    versao3

--- src/si5/univas/view/TelaCadastroProduto.java
+++ src/si5/univas/view/TelaCadastroProduto.java
@@ -1,5 +1,6 @@
 package si5.univas.view;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
@@ -17,50 +18,68 @@ import si5.univas.model.GBC;
 public class TelaCadastroProduto extends JFrame {
 	
 	private JPanel pnGridBag = new JPanel();
+	private JPanel pnBorder = new JPanel();
+	private JPanel pnFlow = new JPanel();
+	private JPanel pnButton = new JPanel();
 	private Controller control;
 	
 	public TelaCadastroProduto(Controller control) {
 		this.control = control;
 	}
 	
+	private JLabel lbPesquisar = new JLabel("Pesquisar:");
 	private JLabel lbNomeProduto = new JLabel("Nome produto:");
 	private JLabel lbQtdProduto = new JLabel("Quantidade produto:");
 	
+	private JTextField txPesquisar = new JTextField(10);
 	private JTextField txNomeProduto = new JTextField(10);
 	private JTextField txQtdProduto = new JTextField(10);
 	
+	private JButton btPesquisar = new JButton("Buscar produto");
 	private JButton btCadastro = new JButton("Cadastrar");
 	private JButton btVoltar = new JButton("Voltar");
 	
 	public void tela(){
 		buttonVoltar();
 		pnGridBag.setLayout(new GridBagLayout());
-		setContentPane(pnGridBag);
+		pnFlow.setLayout(new GridBagLayout());
+		pnButton.setLayout(new GridBagLayout());
+		pnBorder.setLayout(new BorderLayout());
+		setContentPane(pnBorder);
+		pnBorder.add(pnGridBag,BorderLayout.CENTER);
+		pnBorder.add(pnButton,BorderLayout.SOUTH);
+		pnBorder.add(pnFlow,BorderLayout.NORTH);
 		setPreferredSize(new Dimension(400,300));
-		setTitle("Cadastrar Produtos");
+		setTitle("Cadastro e pesquisa de produtos");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setResizable(false);
 		setLocationRelativeTo(null);
 		pack();
 		
-		GBC lbNome = new GBC(1,1).setSpan(1,1);
-		GBC lbQtd = new GBC(1,2).setSpan(1,1);
+		GBC lbPesquisa = new GBC(1,0).setSpan(1,1).setInsets(5,5,5,5).setIpad(7,7);
+		GBC lbNome = new GBC(1,1).setSpan(1,1).setInsets(5,5,5,5).setIpad(7,7);
+		GBC lbQtd = new GBC(1,2).setSpan(1,1).setInsets(5,5,5,5).setIpad(7,7);
 
+		GBC txPesquisa = new GBC(2,0).setSpan(1,1).setInsets(5,5,5,5).setIpad(7,7);
+		GBC txNome = new GBC(2,1).setSpan(1,1).setInsets(5,5,5,5).setIpad(7,7);
+		GBC txQtd = new GBC(2,2).setSpan(1,1).setInsets(5,5,5,5).setIpad(7,7);
+
+		GBC btvoltar = new GBC(1,8).setInsets(5,5,5,5).setIpad(7,7);
+		GBC btcadastrar = new GBC(2,8).setInsets(5,5,5,5).setIpad(7,7);
+		GBC btBuscar = new GBC(1,1).setInsets(3,3,3,3).setIpad(4,4).setSpan(2,1);
 		
-		GBC txNome = new GBC(2,1).setSpan(1,1);
-		GBC txQtd = new GBC(2,2).setSpan(1,1);
-
-		GBC btvoltar = new GBC(1,3).setInsets(5,8,8,8);
-		GBC btcadastrar = new GBC(2,3).setInsets(5,8,8,8);
+		pnFlow.add(lbPesquisar,lbPesquisa);
+		pnFlow.add(txPesquisar, txPesquisa);
 		
 		pnGridBag.add(lbNomeProduto,lbNome);
 		pnGridBag.add(txNomeProduto, txNome);
 		
 		pnGridBag.add(lbQtdProduto,lbQtd);
 		pnGridBag.add(txQtdProduto, txQtd);
-				
-		pnGridBag.add(btCadastro, btcadastrar);
-		pnGridBag.add(btVoltar, btvoltar);	
+		
+		pnFlow.add(btPesquisar, btBuscar);
+		pnButton.add(btCadastro, btcadastrar);
+		pnButton.add(btVoltar, btvoltar);	
 	}
 	
 	public void buttonVoltar(){
